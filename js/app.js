c12a8872d58152904bee455b2ceb3ad6b27efac2
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2023-05-04 16:18:38 +0200

    add audio

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
-$(() => {
-	//jQuery code
-	console.info("jQuery has been loaded !");
-});
+const audio = document.getElementById("starwarstheme");
+setTimeout(function() {
+  audio.play();
+}, 7000);
+
+function togglePlay() {
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
+}
+
+
