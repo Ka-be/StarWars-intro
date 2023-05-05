a93310b9ef851d933241551f4301e55f90f26619
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2023-05-05 11:57:28 +0200

    add volume toggle btn

--- js/app.js
+++ js/app.js
@@ -5,14 +5,15 @@ setTimeout(function() {
 
 audio.volume = 1;
 
-  function toggleMute() {
-    if (audio.volume === 0) {
-      // Si le volume est à 0, rétablir le volume à 1
-      audio.volume = 1;
-    } else {
-      // Sinon, mettre le volume à 0
-      audio.volume = 0;
-    }
+function toggleMute() {
+  let btn = document.getElementById("audio-btn");
+  if (audio.volume === 0) {
+    audio.volume = 1;
+    btn.setAttribute("src", "/img/volume_on.svg");
+  } else {
+    audio.volume = 0;
+    btn.setAttribute("src", "/img/volume_off.svg");
   }
+}
 
 
