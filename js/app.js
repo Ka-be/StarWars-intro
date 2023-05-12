883b2cb35e87687ebd36ef34f0fa31904ff2d258
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2023-05-12 10:48:33 +0200

    Add condition for volume

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 // Allow Audio
+const fxDeploy = document.getElementById("fx-deploy");
+fxDeploy.volume = 0.3;
+const fxRun = document.getElementById("fx-run");
+fxRun.volume = 0.3;
+const fxRetract = document.getElementById("fx-retract");
+fxRetract.volume = 0.3;
 
 window.addEventListener("load", () => {
 	const popup = document.createElement("div");
@@ -15,11 +21,6 @@ window.addEventListener("load", () => {
 
 	document.body.appendChild(popup);
 
-
-
-
-
-
 	const audioControl = document.querySelector(".audio-control");
 	const audio = document.getElementById("starwarstheme");
 
@@ -32,7 +33,6 @@ window.addEventListener("load", () => {
 	const popupAllow = document.getElementById("popup-allow");
 	popupAllow.addEventListener("click", () => {
 		popup.remove();
-		audioControl.style.display = "block";
 		audioControl.classList.remove("paused");
 		setTimeout(function () {
 			audio.play();
@@ -60,77 +60,78 @@ window.addEventListener("load", () => {
 	audioControl.addEventListener("click", function () {
 		if (audio.volume === 0) {
 			audio.volume = 1;
+			fxDeploy.volume = 0.3;
+			fxRun.volume = 0.3;
+			fxRetract.volume = 0.3;
 			audioBtn.src = "/img/volume_on.svg";
 		} else {
 			audio.volume = 0;
+			fxDeploy.volume = 0;
+			fxRun.volume = 0;
+			fxRetract.volume = 0;
 			audioBtn.src = "/img/volume_off.svg";
 		}
 	});
 });
 
-
 // Lightsaber cursor
 
-const fxDeploy = document.getElementById("fx-deploy");
-const fxRun = document.getElementById("fx-run");
-const fxRetract = document.getElementById("fx-retract");
-
 function soundDeploy() {
-  fxDeploy.currentTime = 0;
-  fxDeploy.play();
+	fxDeploy.currentTime = 0;
+	fxDeploy.play();
 }
 
 function soundRun() {
-  fxRun.currentTime = 0;
-  fxRun.play();
+	fxRun.currentTime = 0;
+	fxRun.play();
 }
 
 function soundRetract() {
-  fxRetract.currentTime = 0;
-  fxRetract.play();
+	fxRetract.currentTime = 0;
+	fxRetract.play();
 }
 
 const cursor = document.getElementById("cursor");
 let isMouseDown = false;
 
 window.addEventListener("mousemove", function (e) {
-  if (!isMouseDown) {
-    cursor.style.left = e.clientX + "px";
-    cursor.style.top = e.clientY + "px";
-  }
+	if (!isMouseDown) {
+		cursor.style.left = e.clientX + "px";
+		cursor.style.top = e.clientY + "px";
+	}
 });
 
 window.addEventListener("mousedown", function (e) {
-  if (e.button === 0) {
-    isMouseDown = true;
-    cursor.style.animation = "deployCursor 0.5s forwards";
-    cursor.style.content = "url('/img/lightsaber-full.png')";
-    cursor.style.left = e.clientX + "px";
-    cursor.style.top = e.clientY + "px";
-    soundDeploy(); // Jouer le son fxDeploy
-    fxRun.loop = true; // Activer la lecture en boucle pour fxRun
-    soundRun(); // Jouer le son fxRun
-  }
+	if (e.button === 0) {
+		isMouseDown = true;
+		cursor.style.animation = "deployCursor 0.5s forwards";
+		cursor.style.content = "url('/img/lightsaber-full.png')";
+		cursor.style.left = e.clientX + "px";
+		cursor.style.top = e.clientY + "px";
+		soundDeploy(); // Jouer le son fxDeploy
+		fxRun.loop = true; // Activer la lecture en boucle pour fxRun
+		soundRun(); // Jouer le son fxRun
+	}
 });
 
 window.addEventListener("mouseup", function (e) {
-  if (e.button === 0) {
-    isMouseDown = false;
-    cursor.style.animation = "retractCursor 0.5s forwards";
-    cursor.style.content = "url('/img/lightsaber.png')";
-    fxRun.loop = false; // Désactiver la lecture en boucle pour fxRun
+	if (e.button === 0) {
+		isMouseDown = false;
+		cursor.style.animation = "retractCursor 0.5s forwards";
+		cursor.style.content = "url('/img/lightsaber.png')";
+		fxRun.loop = false; // Désactiver la lecture en boucle pour fxRun
 		fxRun.pause();
 		fxRun.currentTime = 0;
-    soundRetract(); // Jouer le son fxRetract
-  }
+		soundRetract(); // Jouer le son fxRetract
+	}
 });
 
 window.addEventListener("mouseout", function (e) {
-  if (isMouseDown) {
-    isMouseDown = false;
-    cursor.style.animation = "retractCursor 0.5s forwards";
-    cursor.style.content = "url('/img/lightsaber.png')";
-    fxRun.loop = false; // Désactiver la lecture en boucle pour fxRun
-    soundRetract(); // Jouer le son fxRetract
-  }
+	if (isMouseDown) {
+		isMouseDown = false;
+		cursor.style.animation = "retractCursor 0.5s forwards";
+		cursor.style.content = "url('/img/lightsaber.png')";
+		fxRun.loop = false; // Désactiver la lecture en boucle pour fxRun
+		soundRetract(); // Jouer le son fxRetract
+	}
 });
